1fc84f1f8dca507f86738a1d304574d6ce70b791
SlowerPhoton <romansolar@email.cz>
2016-12-14 20:41:35 +0100

    more shrink functions

--- Projects/Resize/resize.py
+++ Projects/Resize/resize.py
@@ -62,8 +62,8 @@ def resize(bmp_file, output_file, new_height, new_width):
     naive_enlarge(bmp_file, output_file, new_height//height_gcd, new_width//width_gcd)
     naive_shrink(output_file, output_file, inp.biHeight//height_gcd, inp.biWidth//width_gcd)
 
-# deprecated 
-def weigthed_mean_shrink(bmp_file, output_file, h, w):
+# 
+def weigthed_mean_shrink_sqrt(bmp_file, output_file, h, w):
     inp = BMP.BMP(bmp_file, 'r')
     inp.raw_to_data()
 
@@ -82,4 +82,46 @@ def weigthed_mean_shrink(bmp_file, output_file, h, w):
     inp.data = new_data
     inp.data_to_raw()
     inp.update_params()
-    inp.writeData(output_file)
+    inp.write_data(output_file)
+
+def weigthed_mean_shrink_frac(bmp_file, output_file, h, w):
+    inp = BMP.BMP(bmp_file, 'r')
+    inp.raw_to_data()
+
+    cols = []
+    for c in range (inp.biWidth//w):
+        #new_col = np.zeros((inp.biHeight, 3), np.uint8)
+        #new_col.reshape((inp.biHeight, 3))
+        new_col = 0
+        for i in range (w):
+            new_col += inp.data[:,c*w+i].astype('uint16')*(1/(2*(w+1)))
+        new_col //= w
+        cols.append(new_col.astype('uint8'))
+    new_data = np.column_stack(cols)
+    new_data = new_data.reshape(inp.biHeight, inp.biWidth//w, 3)  
+    
+    inp.data = new_data
+    inp.data_to_raw()
+    inp.update_params()
+    inp.write_data(output_file)
+
+def geometric_mean_shrink(bmp_file, output_file, h, w):
+    inp = BMP.BMP(bmp_file, 'r')
+    inp.raw_to_data()
+
+    cols = []
+    for c in range (inp.biWidth//w):
+        #new_col = np.zeros((inp.biHeight, 3), np.uint8)
+        #new_col.reshape((inp.biHeight, 3))
+        new_col = 0
+        for i in range (w):
+            new_col *= inp.data[:,c*w+i].astype('uint32')
+        new_col = new_col**(1/w)
+        cols.append(new_col.astype('uint8'))
+    new_data = np.column_stack(cols)
+    new_data = new_data.reshape(inp.biHeight, inp.biWidth//w, 3)  
+    
+    inp.data = new_data
+    inp.data_to_raw()
+    inp.update_params()
+    inp.write_data(output_file)
--- Projects/Resize/test.py
+++ Projects/Resize/test.py
@@ -9,7 +9,7 @@ import numpy as np
 #rs.resize('color_stripes.bmp', 'pad_test.bmp', 30, 10)
 
 #bmp = BMP.BMP('collision.bmp', 'r')
-gen.color_stripes()
+#gen.color_stripes()
 #for i in range(10):
 #    rs.naive_enlarge('color_stripes.bmp', 'mega_stripes.bmp', 5, 5)
 #    rs.naive_shrink('mega_stripes.bmp', 'color_stripes.bmp', 5, 5)
